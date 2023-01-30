d8f1b90951d21a3a95bdcd52b154917216a35d55
Cory <89984104+InTheHouse23@users.noreply.github.com>
2023-01-29 21:08:06 -0500

    Enemy Movement Added
    Enemies now turn and move forward toward the gameobject stored in the moveTarget variable. I have also written an example on how this can be manipulated by throwing together a simple player aggro feature. The enemy now always moves toward the control panel unless the player object is in aggro range. Added a scripts folder.

--- Realtime Project/Assets/Scripts/Spawner.cs
+++ Realtime Project/Assets/Scripts/Spawner.cs
@@ -8,7 +8,6 @@ public class Spawner : MonoBehaviour
     public GameObject enemy;
 
     public float spawnCooldown= 1f;
-
     private float lastSpawnTime = 0f;
 
     private Vector3 spawnLocation;
