5230d1c29d38f463b55dcd0263cdd796c9267d88
JdP100 <99205697+HamsterJones1@users.noreply.github.com>
2023-02-09 09:39:06 -0500

    Added hurtboxes to Traps
    as stated

--- Realtime Project/Assets/Scripts/TrapBehavior.cs
+++ Realtime Project/Assets/Scripts/TrapBehavior.cs
@@ -12,45 +12,17 @@ public class TrapBehavior : MonoBehaviour
     }
 
     public trapType trapVariant;
-    private SphereCollider hurtbox;
-
-    public GameObject hurtboxDebug;
+    public GameObject hurtbox;
+    public float damage = 1.0f;
 
 
     void Start()
     {
-        hurtbox = GetComponent<SphereCollider>();
-        Vector3 newPos = transform.position;
-        switch (trapVariant)
-        {
 
-            case trapType.Tesla:
-                newPos.y = Physics.gravity.y * 10.0f;
-                //SetHurtbox(newPos, 4.0f);
-                break;
-            case trapType.Laser:
-                newPos.y = Physics.gravity.y * 10.0f;
-                SetHurtbox(newPos, 3.0f);
-                break;
-            case trapType.Spike:
-                SetHurtbox(newPos, 4.0f);
-                break;
-            default:
-                break;
-        }
     }
 
     void Update()
     {
 
-    }
-
-    void SetHurtbox(Vector3 pos, float radius)
-    {
-        hurtbox.transform.position = pos;
-        hurtbox.radius = radius;
-
-        hurtboxDebug.transform.position = pos;
-        hurtboxDebug.transform.localScale = new Vector3(radius, radius, radius);    
     }
 }
