0e17ae8b06371711615debd8151b96996ad2fe42
JdP100 <99205697+HamsterJones1@users.noreply.github.com>
2023-02-09 09:50:46 -0500

    changed one line of code
    added a timer

--- Realtime Project/Assets/Scripts/TrapBehavior.cs
+++ Realtime Project/Assets/Scripts/TrapBehavior.cs
@@ -15,13 +15,26 @@ public class TrapBehavior : MonoBehaviour
     public GameObject hurtbox;
     public float damage = 1.0f;
 
+    public float setDamageTimer;
+    private float timer;
+
 
     void Start()
     {
-
+        timer = setDamageTimer;
     }
 
     void Update()
+    {
+        // Deal damage
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            dealDamage();
+        }
+    }
+
+    void dealDamage()
     {
 
     }
