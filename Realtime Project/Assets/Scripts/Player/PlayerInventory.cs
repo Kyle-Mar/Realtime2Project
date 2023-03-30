325265c6971499117780a44bdb856c576e9d4272
Kyle Mar <33503006+Kyle-Mar@users.noreply.github.com>
2023-03-30 10:41:57 -0400

    Inventory drawing on Screen
    No item count yet....
    
    Still need to remove items when placing a trap...

--- Realtime Project/Assets/Scripts/Player/PlayerInventory.cs
+++ Realtime Project/Assets/Scripts/Player/PlayerInventory.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 using TMPro;
 public class PlayerInventory : MonoBehaviour
 {
-    public static float numCoins = 0;
-    public TMP_Text coinsText;
-    List<InventorySlot> inventory = new List<InventorySlot>();
     const int MAX_ITEMS_PER_TYPE = 5;
     const int MAX_ITEMS = 3;
+    
+    public static float numCoins = 0;
+    public TMP_Text coinsText;
+
+    List<InventorySlot> inventory = new List<InventorySlot>();
     public TrapItem testItem;
+
+    public GameObject canvasObject;
+    public GameObject inventoryHudIcon;
+    List<GameObject> inventoryIcons = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +29,7 @@ public class PlayerInventory : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.L)){
+            DrawInventory();
             foreach (var item in inventory)
             {
                 Debug.Log(item.count + item.item.name);
@@ -81,6 +89,43 @@ public class PlayerInventory : MonoBehaviour
             }
             index++;
         }
+    }
+
+    public void DrawInventory()
+    {
+        for(int i = 0; i < inventoryIcons.Count; i++)
+        {
+            Destroy(inventoryIcons[i]);
+        }
+
+        RectTransform rect = canvasObject.GetComponent<RectTransform>();
+
+        float height = rect.sizeDelta.y * rect.localScale.y;
+        float width = rect.sizeDelta.x * rect.localScale.x;
+
+
+        Vector3 startPos = new Vector3(width/20, height - height /10, 0f);
+        Vector3 iconPos = startPos;
+        int index = 1;
+        foreach(InventorySlot slot in inventory)
+        {
+            GameObject iconObject = Instantiate(inventoryHudIcon, iconPos, Quaternion.identity, canvasObject.transform);
+            Debug.Log(iconObject.transform.position);
+            Image image = iconObject.GetComponent<Image>();
+            TMP_Text text = iconObject.GetComponentInChildren<TMP_Text>();
+
+            image.sprite = slot.item.image;
+            text.text = slot.item.itemName;
+            iconPos.y = startPos.y - (height / 10) * index;
+
+            index++;
+
+            inventoryIcons.Add(iconObject);
+
+        }
+
+
+
     }
 }
 
