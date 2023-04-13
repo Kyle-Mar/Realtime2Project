7b0dcb539394d68416b9fdb4be05828245ee1b48
Kyle Mar <33503006+Kyle-Mar@users.noreply.github.com>
2023-04-13 10:32:56 -0400

    SFX Player

---
+++ Realtime Project/Assets/Scripts/GameSystems/SFXPlayer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SFXPlayer : MonoBehaviour
+{
+    //public AudioMixer mixer;
+    //public AudioMixerGroup mixerGroup;
+
+    void Start()
+    {
+        //mixer = Resources.Load<AudioMixer>("Mixer");
+        //mixerGroup = mixer.FindMatchingGroups("SoundEffects")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        GameObject SFXObject = new GameObject("SoundEffect");
+        Play(SFXObject, clip);
+    }
+
+    //spatial override
+    public void PlaySFX(AudioClip clip, Vector3 position)
+    {
+        GameObject SFXObject = new GameObject("SoundEffect");
+        SFXObject.transform.position = position;
+        Play(SFXObject, clip);
+
+    }
+
+    private void Play(GameObject SFXObject, AudioClip clip)
+    {
+        AudioSource source = SFXObject.AddComponent<AudioSource>();
+        //source.outputAudioMixerGroup = mixerGroup;
+        source.clip = clip;
+        //source.volume = PlayerPrefs.GetFloat("EffectsVolume");
+        //source.pitch = Random.Range(0.75f, 1.0f);
+        source.Play();
+        StartCoroutine(DestroyAfterClipLength(clip.length, SFXObject));
+    }
+
+    IEnumerator DestroyAfterClipLength(float length, GameObject SFXObject)
+    {
+        yield return new WaitForSeconds(length);
+        Destroy(SFXObject);
+    }
+}
